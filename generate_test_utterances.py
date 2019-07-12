5061b6e01f5f2d1d0d0edc3dceb9ebdc71255b80
Daria Shamrai <39339052+DariaSh@users.noreply.github.com>
2019-07-12 13:45:03 +0300

    Add files via upload
    Two usage cases - for train and for test utterances generation

---
+++ generate_test_utterances.py
@@ -0,0 +1,10 @@
+import argparse
+from utterances_generator import UtterancesGenerator
+
+if __name__ == '__main__':
+     parser = argparse.ArgumentParser()
+     parser.add_argument('--input', help='input file', required=True)
+     parser.add_argument('--output', help='output json file', required=True)
+     args = parser.parse_args()
+     generator = UtterancesGenerator(test=True)
+     generator.write_utterances_json(args.input, args.output)
